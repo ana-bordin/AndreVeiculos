51668b0d2da1f82588c5ddb977151a8c2d4e3731
Ana-bordin <anabordin2008@gmail.com>
2024-06-06 10:03:41 -0300

    update data insertion 1.1

---
+++ Controllers/CarController.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using Services;
+
+namespace Controllers
+{
+    public class CarController
+    {
+        private readonly CarService _carService;
+
+        public CarController()
+        {
+            _carService = new CarService();
+        }
+        public bool InsertAll(List<Car> cars)
+        {
+            return _carService.InsertAll(cars);
+        }
+        public bool Insert(Car car)
+        {
+            return _carService.Insert(car);
+        }
+        public List<Car> GetAll()
+        {
+            return _carService.GetAll();
+        }
+    }
+}
---
+++ Services/CarService.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+using Repositories;
+
+namespace Services
+{
+    public class CarService
+    {
+        ICarRepository _carRepository;
+
+        public CarService()
+        {
+            _carRepository = new CarRepository();
+        }
+
+        public bool InsertAll(List<Car> cars)
+        {
+            return _carRepository.InsertAll(cars);
+        }
+
+        public bool Insert(Car car)
+        {
+            return _carRepository.Insert(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carRepository.GetAll();
+        }
+    }
+}
